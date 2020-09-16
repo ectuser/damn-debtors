52b5d50e1952bd7f394562f8e0c3dd54ab3f8d76
ectuser <m.ectuser@gmail.com>
2020-09-16 23:57:40 +0700

    Started doing the sign in form

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.scss']
+  styleUrls: ['./sign-in.component.scss'],
 })
 export class SignInComponent implements OnInit {
-
-  constructor() { }
+  signInForm: FormGroup;
+  constructor() {}
 
   ngOnInit(): void {
+    this.signInForm = new FormGroup({
+      email: new FormControl(),
+      password: new FormControl(),
+    });
   }
-
 }
