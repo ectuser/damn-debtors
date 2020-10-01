fc2f7fc07f6f3be513b0e31385187cd61b0ef729
ectuser <m.ectuser@gmail.com>
2020-10-01 12:24:25 +0700

    Added changing password and email endpoints, forced them to work

--- server/api/routes/users.js
+++ server/api/routes/users.js
@@ -1,4 +1,15 @@
 "use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -42,6 +53,7 @@ var bodyParser = require('body-parser');
 var jwt = require('jsonwebtoken');
 var dbConnection_1 = require("../../db/dbConnection");
 var passport_1 = require("../../passport");
+var securityService_1 = require("../../services/securityService");
 // usersRouter.get(
 //   '/:userId',
 //   passport.authenticate('jwt', { session: false }),
@@ -69,7 +81,6 @@ usersRouter.get('/profile', passport_1["default"].authenticate('jwt', { session:
                     return [2 /*return*/];
                 }
                 userId = req.user.id;
-                console.log(req.user);
                 return [4 /*yield*/, dbConnection_1.usersDb.findOne({ id: userId })];
             case 1:
                 dbUser = _a.sent();
@@ -87,6 +98,87 @@ usersRouter.get('/profile', passport_1["default"].authenticate('jwt', { session:
                     amountOfMoney: amountOfMoney
                 };
                 res.json(obj);
+                return [2 /*return*/];
+        }
+    });
+}); });
+usersRouter.put('/password', passport_1["default"].authenticate('jwt', { session: false }), function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+    var userId, dbUser, result, salt, encryptedPassword, updatePasswordResult;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                if (!req.body.oldPassword || !req.body.newPassword) {
+                    res.status(400).json({ message: 'Bad request' });
+                    return [2 /*return*/];
+                }
+                if (req.body.oldPassword === req.body.newPassword) {
+                    res
+                        .status(400)
+                        .json({ message: 'Old and new passwords should not match' });
+                    return [2 /*return*/];
+                }
+                if (!req.user || !req.user.id) {
+                    res.status(404).json({ message: "Can't find user" });
+                    return [2 /*return*/];
+                }
+                userId = req.user.id;
+                return [4 /*yield*/, dbConnection_1.usersDb.findOne({ id: userId })];
+            case 1:
+                dbUser = _a.sent();
+                if (!dbUser) {
+                    res.status(404).json({ message: 'User not found' });
+                    return [2 /*return*/];
+                }
+                result = securityService_1.checkThePassword(req.body.oldPassword, dbUser.password, dbUser.salt);
+                if (!result) {
+                    res.status(403).json({ message: 'Wrong password' });
+                    return [2 /*return*/];
+                }
+                salt = securityService_1.generateSalt();
+                encryptedPassword = securityService_1.encryptPassword(req.body.newPassword, salt);
+                return [4 /*yield*/, dbConnection_1.usersDb.update({ id: dbUser.id }, __assign(__assign({}, dbUser), { password: encryptedPassword, salt: salt }))];
+            case 2:
+                updatePasswordResult = _a.sent();
+                if (updatePasswordResult) {
+                    res.json({ message: 'Password updated' });
+                }
+                else {
+                    res.status(500).json({ message: 'Something went wrong' });
+                }
+                return [2 /*return*/];
+        }
+    });
+}); });
+usersRouter.put('/profile', passport_1["default"].authenticate('jwt', { session: false }), function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+    var userId, dbUser, result;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                if (!req.body.email) {
+                    res.status(400).json({ message: 'Bad request' });
+                    return [2 /*return*/];
+                }
+                if (!req.user || !req.user.id) {
+                    res.status(404).json({ message: "Can't find user" });
+                    return [2 /*return*/];
+                }
+                userId = req.user.id;
+                return [4 /*yield*/, dbConnection_1.usersDb.findOne({ id: userId })];
+            case 1:
+                dbUser = _a.sent();
+                if (!dbUser) {
+                    res.status(404).json({ message: 'User not found' });
+                    return [2 /*return*/];
+                }
+                return [4 /*yield*/, dbConnection_1.usersDb.update({ id: userId }, __assign(__assign({}, dbUser), { email: req.body.email }))];
+            case 2:
+                result = _a.sent();
+                if (result) {
+                    res.json({ message: 'Profile updated' });
+                }
+                else {
+                    res.status(500).json({ message: 'Something went wrong' });
+                }
                 return [2 /*return*/];
         }
     });
